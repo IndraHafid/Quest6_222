9b992d709e763dba5671c8baf29f1dae4499a603
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 15:33:52 +0700

    membuat Scaffold dan NavHost untuk navigasi UI

--- app/src/main/java/com/example/myarsitektur8/view/uicontroller/navigasi.kt
+++ app/src/main/java/com/example/myarsitektur8/view/uicontroller/navigasi.kt
@@ -1,11 +1,16 @@
 package com.example.myarsitektur8.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.myarsitektur8.viewmodel.SiswaViewModel
 
@@ -14,6 +19,7 @@ enum class PengelolaHalaman {
     Detail
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
     viewModel: SiswaViewModel = viewModel(),
@@ -22,5 +28,12 @@ fun SiswaApp(
     val uiState by viewModel.statusUI.collectAsState()
     val context = LocalContext.current
 
-
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = PengelolaHalaman.Formulir.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+        }
+    }
 }
