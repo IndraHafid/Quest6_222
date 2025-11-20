ac22fae8a96cf271fad92d5d531a6c2dc500f57d
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 15:07:35 +0700

    membuat parameter pilihanJK dan state untuk form input

--- app/src/main/java/com/example/myarsitektur8/view/FromSiswa.kt
+++ app/src/main/java/com/example/myarsitektur8/view/FromSiswa.kt
@@ -1,9 +1,22 @@
 package com.example.myarsitektur8.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by rememberSaveable { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+
 
 }
