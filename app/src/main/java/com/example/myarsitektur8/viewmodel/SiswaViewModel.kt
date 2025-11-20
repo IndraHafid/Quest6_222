42cd3274d249b69f654711007ab563d253937ad4
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 14:55:14 +0700

    membuat fungsi setSiswa untuk update data Siswa

--- app/src/main/java/com/example/myarsitektur8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitektur8/viewmodel/SiswaViewModel.kt
@@ -9,5 +9,7 @@ import kotlinx.coroutines.flow.asStateFlow
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+    fun setSiswa(listDataSiswa: List<String>) {
 
+    }
 }
