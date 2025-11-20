c892817d0c84bf0ece090f4f479b5572bda260c4
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 15:20:51 +0700

    menambahkan Scaffold dan TopAppBar dengan back button

--- app/src/main/java/com/example/myarsitektur8/view/TampilanSiswa.kt
+++ app/src/main/java/com/example/myarsitektur8/view/TampilanSiswa.kt
@@ -1,9 +1,22 @@
 package com.example.myarsitektur8.view
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.myarsitektur8.R
 import com.example.myarsitektur8.model.Siswa
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilSiswa(
     statusUISiswa: Siswa,
@@ -11,4 +24,26 @@ fun TampilSiswa(
     modifier: Modifier = Modifier
 ) {
 
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Detail Siswa", color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                ),
+                navigationIcon = {
+                    IconButton(onClick = onBackButtonClicked) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        }
+    ) { padding ->
+
+    }
 }
