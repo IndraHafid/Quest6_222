c14d3ee8acd01f2b31d4f11f35787e46e6200f5e
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 15:19:00 +0700

    membuat parameter statusUISiswa dan onBackButtonClicked

--- app/src/main/java/com/example/myarsitektur8/view/TampilanSiswa.kt
+++ app/src/main/java/com/example/myarsitektur8/view/TampilanSiswa.kt
@@ -1,9 +1,14 @@
 package com.example.myarsitektur8.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.myarsitektur8.model.Siswa
 
 @Composable
 fun TampilSiswa(
+    statusUISiswa: Siswa,
+    onBackButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
 
 }
