f3e18fe06905fff2bf2e4dfea222bf1ad64ab6aa
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 14:40:43 +0700

    membuat file DataJK object untuk list jenis kelamin

---
+++ app/src/main/java/com/example/myarsitektur8/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitektur8.model
+
+import com.example.myarsitektur8.R
+
+object DataJK {
+    val jenisJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
