236630c33d3493d7064e5e7b692fe8e4bf0924b1
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 15:32:17 +0700

    menambah SiswaApp composable dengan viewModel dan navController

--- app/src/main/java/com/example/myarsitektur8/view/uicontroller/navigasi.kt
+++ app/src/main/java/com/example/myarsitektur8/view/uicontroller/navigasi.kt
@@ -1,8 +1,26 @@
 package com.example.myarsitektur8.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.myarsitektur8.viewmodel.SiswaViewModel
+
 enum class PengelolaHalaman {
     Formulir,
     Detail
 }
 
+@Composable
+fun SiswaApp(
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val uiState by viewModel.statusUI.collectAsState()
+    val context = LocalContext.current
 
+
+}
