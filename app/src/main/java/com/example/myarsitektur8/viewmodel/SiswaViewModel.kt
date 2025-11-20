e86595941a718911be45ce8eecc2b6688e093a89
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-20 14:56:03 +0700

    menambahkan implement copy() untuk update data siswa secara aman

--- app/src/main/java/com/example/myarsitektur8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitektur8/viewmodel/SiswaViewModel.kt
@@ -5,11 +5,18 @@ import com.example.myarsitektur8.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
     fun setSiswa(listDataSiswa: List<String>) {
-
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = listDataSiswa.getOrNull(0) ?: "",
+                gender = listDataSiswa.getOrNull(1) ?: "",
+                alamat = listDataSiswa.getOrNull(2) ?: ""
+            )
+        }
     }
 }
